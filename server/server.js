de67f31747a93c2444a66562af16f1c3eeb7f565
Jahongir <jahongirabdulloev06@gmail.com>
2026-03-12 14:04:39 +0500

    Added the variable that to stop/run the app

--- server/server.js
+++ server/server.js
@@ -28,3 +28,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+
+if (process.env.DISABLE === "true") {
+  console.log("Server disabled");
+  process.exit(0);
+}
